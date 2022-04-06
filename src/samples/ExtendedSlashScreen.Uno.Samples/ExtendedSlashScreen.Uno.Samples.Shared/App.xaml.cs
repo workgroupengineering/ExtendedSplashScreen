97e1e90fdff2e13081c7e25a1ed410ca060b3e6d
Marc-Antoine Therrien <ma.therrien@nventive.com>
2022-04-06 13:05:11 -0400

    feat: Align app startup with UnoAppTemplate

--- src/samples/ExtendedSlashScreen.Uno.Samples/ExtendedSlashScreen.Uno.Samples.Shared/App.xaml.cs
+++ src/samples/ExtendedSlashScreen.Uno.Samples/ExtendedSlashScreen.Uno.Samples.Shared/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -37,6 +38,10 @@ namespace ExtendedSlashScreen.Uno.Samples
 
 		public Shell Shell { get; private set; }
 
+		public Window CurrentWindow => Windows.UI.Xaml.Window.Current;
+
+		public Activity ShellActivity { get; } = new Activity(nameof(Shell));
+
 		/// <summary>
 		/// Invoked when the application is launched normally by the end user.  Other entry points
 		/// will be used such as when the application is launched to open a specific file.
@@ -49,17 +54,22 @@ namespace ExtendedSlashScreen.Uno.Samples
 			{
 				// this.DebugSettings.EnableFrameRateCounter = true;
 			}
-#endif
-            Shell = Windows.UI.Xaml.Window.Current.Content as Shell;
+# endif
+			Shell = CurrentWindow.Content as Shell;
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (Shell == null)
-            {
-				Windows.UI.Xaml.Window.Current.Content = Shell = new Shell(e);
+			var isFirstLaunch = Shell == null;
+
+			if (isFirstLaunch)
+			{
+				ShellActivity.Start();
+
+				CurrentWindow.Content = Shell = new Shell(e);
+
+				ShellActivity.Stop();
 			}
 
-			Windows.UI.Xaml.Window.Current.Activate();
+			CurrentWindow.Activate();
+			Shell = Windows.UI.Xaml.Window.Current.Content as Shell;
 
 			if (Shell.NavigationFrame.Content == null)
 			{
