9466070f94acf8fd21b4a1c38a7dfa07ba8b871c
Marc-Antoine Soucy <ma.soucy@nventive.com>
2022-11-16 10:52:38 -0500

    feat: added support for WinUI in the library
    a
    
    a
    
    fix: added important files to GitIgnore
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a1

---
+++ src/library/ExtendedSplashScreen.Uno.WinUI/ExtendedSplashScreen.Windows.cs
@@ -0,0 +1,17 @@
+﻿#if WINDOWS
+using Windows.ApplicationModel.Activation;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Nventive.ExtendedSplashScreen
+{
+	public partial class ExtendedSplashScreen
+	{
+		public FrameworkElement GetNativeSplashScreen(SplashScreen splashScreen)
+		{
+			// Splashscreen is not supported yet on winui https://github.com/microsoft/microsoft-ui-xaml/issues/4055
+			return default(FrameworkElement);
+		}
+	}
+}
+#endif
--- src/library/ExtendedSplashScreen.Uno/ExtendedSplashScreen.Uwp.cs
+++ src/library/ExtendedSplashScreen.Uno/ExtendedSplashScreen.Uwp.cs
@@ -1,29 +1,17 @@
-﻿#if WINDOWS || WINDOWS_UWP || NET472_OR_GREATER
+﻿#if WINDOWS_UWP
 using System;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 using Microsoft.Extensions.Logging;
 using Uno.Extensions;
 using Windows.ApplicationModel.Activation;
 using Windows.Graphics.Display;
-#if WINUI
-using Microsoft.UI;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Markup;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Imaging;
-using Windows.UI;
-#else
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-#endif
 
 namespace Nventive.ExtendedSplashScreen
 {
